2a27f12246f5378cd680b98d971311de36f45065
Izabela Siemaszko <izabela.siemaszko@onet.pl>
2020-03-14 20:45:49 +0100

    fixed ex2

--- BSK/PS2-3/BSKPS01_02/Zadanie2_IS.cs
+++ BSK/PS2-3/BSKPS01_02/Zadanie2_IS.cs
@@ -72,103 +72,113 @@ namespace BSKPS01_02
 
         }
 
-         public static string Decypher(string text, string key, string n)
-          {
-           text = text.ToUpper();
-              key = key.ToUpper();
-              int d = int.Parse(n);
-              int j = 0;
+        public static string Decypher(string text, string key, string n)
+        {
+            text = text.ToUpper();
+            key = key.ToUpper();
+            int d = int.Parse(n);
 
-               int[] tab_key = new int[d];
+            int k = 0;
+            int[] tab_key = new int[d];
             int[] pom_key = new int[d];
             string dtext = "";
 
-              for (int i = 0; i < key.Length; i++)
-              {
-                  if (!key[i].Equals('-'))
-                  {
-                      tab_key[j] = int.Parse(key[i].ToString());
-                    pom_key[j]= int.Parse(key[i].ToString());
-                    j++;
-                  }
-              }
-           
-            Array.Reverse(tab_key);//odwrócenie tablicy  
-
-           
-
-            int resultMod = text.Length % d;
-
-              String[] row = new String[d];
-              j = 0;
-              for (int i = 0; i < text.Length; i++)
-              {
-                  row[j] = text[i].ToString();
-                  j++;
-                  if (j % d == 0)
-                  {
-
-                      for (int k = 0; k < tab_key.Length; k++)
-                      {
-                          dtext += row[tab_key[k] - 1];
-                      }
-
-
-                      j = 0;
-                      row = new String[d];
-                  }
-
-              }
-
-              if (resultMod != 0)
-              {
-
-                  row = new string[d];
-                  j = 0;
-              
-                  for (int i = text.Length - resultMod; i < text.Length; i++)
-                  {
-                      row[j] = text[i].ToString();
-                      j++;
-                  }
-
-                String[] pom = new string[d];
-                int p = d - resultMod;//1 bo to 4-3
-                j = 0;
-                for (int i = 1; i < p + 1; i++)
+            for (int i = 0; i < key.Length; i++)
+            {
+                if (!key[i].Equals('-'))
                 {
-                    
-                    for(int l = 0; l < pom_key.Length; l++)
+                    tab_key[k] = int.Parse(key[i].ToString());
+                    pom_key[k] = int.Parse(key[i].ToString());
+                    k++;
+                }
+            }
+            
+            int pivot = 0;
+            int size = (int)Math.Ceiling((double)text.Length / (int)d);
+            int rozmiarWierszaOstatniego = text.Length % d;
+            char?[,] matrix = new char?[d, size];
+             char[] tab = new char[rozmiarWierszaOstatniego];
+            if (rozmiarWierszaOstatniego != 0)
+            {
+                for (int i = 0; i < size - 1; i++)
+                {
+                    for (int j = 0; j < d; j++)
                     {
-                        if (pom_key[l] == d)
+                        try
                         {
-                            pom[l] = "@@";
-                            d--;
-                            break;
+                            matrix[(pom_key[j] - 1), i] = text[j + ((d) * pivot)];
+                        }
+                        catch (IndexOutOfRangeException)
+                        {
+
                         }
                     }
-                    
+                    pivot++;
                 }
-                for (int i = 0; i < pom.Length; i++)
+                pivot++;
+                //ostatni wiersz obsluga
+                tab = new char[rozmiarWierszaOstatniego];
+                for(int x=text.Length-rozmiarWierszaOstatniego;x<text.Length;x++)
                 {
-                    if (pom[i] != "@@")
+                    char aktualnaLitera = text[x];
+                    for(int y=0;y<d;y++)
                     {
-                        pom[i] = row[j];
-                        j++;
+                        if(pom_key[y] <= rozmiarWierszaOstatniego && tab[pom_key[y] - 1]=='\0')
+                        {
+                            try
+                            {
+                                
+                                tab[pom_key[y] - 1] = text[x];
+                                
+                                break;
+                            }
+                            catch(IndexOutOfRangeException)
+                            {
+
+                            }
+                            
+                        }
                     }
                 }
-                j = 0;
-                for (int k = 0; k < tab_key.Length; k++)
-                  {
-                      if (pom[tab_key[k] - 1] != "@@")
-                      {
-                          dtext += pom[tab_key[k] - 1];
-                      }
-                  }
-              }
+                System.Diagnostics.Debug.WriteLine(new string(tab));
+               
+            }
+            else
+            {
+                //char?[,] matrix = new char?[d, size];
+                for (int i = 0; i < size; i++)
+                {
+                    for (int j = 0; j < d; j++)
+                    {
+                        try
+                        {
+                            matrix[(pom_key[j] - 1), i] = text[j + ((d) * pivot)];
+                        }
+                        catch (IndexOutOfRangeException)
+                        {
 
-              return dtext;
-          }
-
-    }
+                        }
+                    }
+                    pivot++;
+                }
+            }
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < d; j++)
+                {
+                    if(matrix[j,i].HasValue)
+                    {
+                        dtext += matrix[j,i];
+                    }
+                }
+            }
+            for (int r = 0; r < tab.Length; r++)
+            {
+                dtext += tab[r];
+            }
+               // System.Diagnostics.Debug.WriteLine(tab[r]);
+            return dtext;
+           
+        }
+        }
 }
