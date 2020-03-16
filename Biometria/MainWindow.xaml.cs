04cfbca544b8c7be09b928df7d0ea4c15b90c92a
Daniel Kamiński <daniel.kaminski1998@gmail.com>
2020-03-16 03:09:37 +0100

    Biometria zad1 i WPF zad4

---
+++ Biometria/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie1
+{
+    public partial class MainWindow : Window
+    {
+        private Bitmap img;
+        Uri fileUri;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Upload_Image(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                fileUri = new Uri(openFileDialog.FileName);
+                imgDynamic.Source = new BitmapImage(fileUri);
+                img = new Bitmap(openFileDialog.FileName);
+            }
+        }
+
+        private void Save_Image(object sender, RoutedEventArgs e)
+        {         
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif|TIF Image|*.tif";
+            saveFileDialog1.Title = "Save an Image File";
+            saveFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            saveFileDialog1.ShowDialog();
+            
+            if (saveFileDialog1.FileName != "")
+            {
+                
+                FileStream fs =
+                    (FileStream)saveFileDialog1.OpenFile();
+                BitmapEncoder encoder;
+                switch (saveFileDialog1.FilterIndex)
+                {
+                    case 1:
+                        encoder = new JpegBitmapEncoder();
+                        encoder.Frames.Add(BitmapFrame.Create((BitmapSource)imgDynamic.Source));
+                        encoder.Save(fs);
+                        break;
+                    case 2:
+                        encoder = new BmpBitmapEncoder();
+                        encoder.Frames.Add(BitmapFrame.Create((BitmapSource)imgDynamic.Source));
+                        encoder.Save(fs);
+                        break;
+                    case 3:
+                        encoder = new GifBitmapEncoder();
+                        encoder.Frames.Add(BitmapFrame.Create((BitmapSource)imgDynamic.Source));
+                        encoder.Save(fs);
+                        break;
+                    case 4:
+                        encoder = new TiffBitmapEncoder();
+                        encoder.Frames.Add(BitmapFrame.Create((BitmapSource)imgDynamic.Source));
+                        encoder.Save(fs);
+                        break;
+
+
+                }
+
+                fs.Close();
+            }
+        }
+      
+           
+
+        private void Pixel_read(object sender, MouseEventArgs e)
+        {
+            System.Windows.Point p = e.GetPosition((IInputElement)e.Source);
+            System.Drawing.Color color = img.GetPixel((int)p.X, (int)p.Y);
+            temp.Content = "RGB(" + color.R + ", " + color.G + ", " + color.B + ")";
+        }
+
+        private void Pixel_change(object sender, MouseButtonEventArgs e)
+        {
+            System.Windows.Point p = e.GetPosition((IInputElement)e.Source);
+            System.Drawing.Color color = img.GetPixel((int)p.X, (int)p.Y);
+            img.SetPixel((int)p.X, (int)p.Y, System.Drawing.Color.Red);
+            MemoryStream ms = new MemoryStream();
+            img.Save(ms, ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+            imgDynamic.Source = image;
+        }
+    }
+}
---
+++ WPF/Zad4/MainWindow.xaml.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Zadanie4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Rectangle lastRectangle;
+        private Point startPoint;
+        private Rectangle rectangle;
+        private double x;
+        private double y;
+        public MainWindow()
+        {
+            lastRectangle = new Rectangle();
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(MoveRectangle);
+            timer.Tick += new EventHandler(ChangeSize);
+            timer.Start();
+        }
+
+        private void Canvas_startDrawing(object sender, MouseButtonEventArgs e)
+        {
+            startPoint = e.GetPosition(mainCanvas);
+            rectangle = new Rectangle
+            {
+                Stroke=Brushes.Black,
+                StrokeThickness = 1
+            };
+            Canvas.SetLeft(rectangle, startPoint.X);
+            Canvas.SetTop(rectangle, startPoint.Y);
+            mainCanvas.Children.Add(rectangle);
+        }
+
+        private void Canvas_endDrawing(object sender, MouseButtonEventArgs e)
+        {
+            lastRectangle = rectangle;
+        }
+
+        private void Canvas_draw(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Released || rectangle == null)
+                return;
+            var position = e.GetPosition(mainCanvas);
+            var x = Math.Min(position.X, startPoint.X);
+            var y = Math.Min(position.Y, startPoint.Y);
+            var w = Math.Max(position.X,startPoint.X)-x;
+            var h = Math.Max(position.Y, startPoint.Y) - y;
+            rectangle.Width = w;
+            rectangle.Height = h;
+            this.x = x;
+            this.y = y;
+            Canvas.SetLeft(rectangle, x);
+            Canvas.SetTop(rectangle, y);
+
+        }
+
+        private void MoveRectangle(object sender, EventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.LeftShift))
+                return;
+            else if (Keyboard.IsKeyDown(Key.Left))
+            {
+                x -= .05;
+                Canvas.SetLeft(lastRectangle, x);
+            }
+            else if (Keyboard.IsKeyDown(Key.Up))
+            {
+                y -= .05;
+                Canvas.SetTop(lastRectangle, y);
+            }
+            else if (Keyboard.IsKeyDown(Key.Down))
+            {
+                y += .05;
+                Canvas.SetTop(lastRectangle, y);
+            }
+            else if (Keyboard.IsKeyDown(Key.Right))
+            {
+                x += .05;
+                Canvas.SetLeft(lastRectangle, x);
+            }
+        }
+
+        private void ChangeSize(object sender, EventArgs e)
+        {
+            if (!Keyboard.IsKeyDown(Key.LeftShift))
+                return;
+            else if (Keyboard.IsKeyDown(Key.Left) && lastRectangle.Width>10 )
+            {
+                lastRectangle.Width -= 0.1;
+                Canvas.SetLeft(lastRectangle, x);
+            }
+            else if (Keyboard.IsKeyDown(Key.Up) && lastRectangle.Height> 10 )
+            {
+                lastRectangle.Height -= 0.1;
+                Canvas.SetTop(lastRectangle, y);
+            }
+            else if (Keyboard.IsKeyDown(Key.Down) && Canvas.GetTop(lastRectangle)+lastRectangle.Height<(mainWindow.Height-40))
+            {
+                lastRectangle.Height += 0.1;
+                Canvas.SetTop(lastRectangle, y);
+            }
+            else if (Keyboard.IsKeyDown(Key.Right) && Canvas.GetLeft(lastRectangle) + lastRectangle.Width < (mainWindow.Width - 20))
+            {
+                lastRectangle.Width += 0.1;
+                Canvas.SetLeft(lastRectangle, x);
+            }
+
+        }
+    }
+}
